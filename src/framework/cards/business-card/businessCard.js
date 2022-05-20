aca9ac3d1a5f1c2563e100d66153c99a2fc4c6d4
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2022-05-21 01:19:08 +0300

    Tiny styling fix
    * Dark theme is more clear now (background and foreground colours swapped)
    
    * Adjusted the theme switch button's shadow to be in line with the other shadows in mobile view.
    
    * Added margin to business card text for mobile view to make sure the edge of the text aren't too close to the screen edges.

--- src/framework/cards/business-card/businessCard.js
+++ src/framework/cards/business-card/businessCard.js
@@ -11,9 +11,9 @@ export default function BusinessCard(props) {
                 {props.data.currentlyAt}
             </div>
             <div id={styles.socialBox}>
-                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={props.data.socials.github} > <i className="fab fa-github"></i> </a>
-                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={props.data.socials.linkedin} > <i className="fab fa-linkedin"></i> </a>
-                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={`mailto:${props.data.socials.email}`} > <i className="fas fa-envelope"></i> </a>
+                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={props.data.socials.github} ><i className="fab fa-github"></i></a>
+                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={props.data.socials.linkedin} ><i className="fab fa-linkedin"></i></a>
+                <a className={styles.socialsItem} rel="noreferrer" target="_blank" href={`mailto:${props.data.socials.email}`} ><i className="fas fa-envelope"></i></a>
             </div>
         </div>
     )
